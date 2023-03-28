31ae4ba07e16e92f61b5cf583c21fa8072b923d3
Liam Tirney <liamtirney7427@gmail.com>
2023-03-28 15:28:32 -0400

    Seed database with climbing gym test data

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Gym = require('../models/gym');
+
+mongoose.connect('mongodb://127.0.0.1:27017/find-a-rock');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+// pass in an array and return a random element from that array
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Gym.deleteMany({});
+    // const g = new Gym({ title: 'purple field' });
+    // await g.save();
+    for(let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000)
+        const gym = new Gym({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await gym.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
