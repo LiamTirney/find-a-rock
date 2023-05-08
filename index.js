10e279b712b66eec149aefedd0fdfcf882836fba
Liam Tirney <liamtirney7427@gmail.com>
2023-05-08 12:31:16 -0400

    Set up public directory to serve static assets

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/gyms', gyms)
 app.use('/gyms/:id/reviews', reviews)
